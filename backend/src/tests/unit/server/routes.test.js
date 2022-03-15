c9bac88994abef4a1af54977ad1df3093b25db17
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-03-15 20:18:49 -0300

    test: set test to do

--- backend/src/tests/unit/server/routes.test.js
+++ backend/src/tests/unit/server/routes.test.js
@@ -1,7 +1,12 @@
 import { describe, test, jest, expect } from '@jest/globals'
 
 describe('Test server routes', () => {
-  test('true', () => {
-    expect(true).toBeTruthy()
-  })
+ test.todo('GET / - should return to home page')
+ test.todo('GET /home - should response with home/index.js')
+ test.todo('GET /controller - should response with controller/index.js')
+ test.todo('GET /file.txt - should response with file stream')
+ test.todo('GET /unknown - should return 404 page')
+
+ test.todo('should return a page with 404')
+ test.todo('given an error it should respond with 500')
 })
