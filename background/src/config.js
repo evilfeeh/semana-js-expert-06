8384752b04104b5bf2868269b84ce5682fad2c4c
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-03-14 21:11:24 -0300

    feat: create config file with enviroment of the application

---
+++ background/src/config.js
@@ -0,0 +1,27 @@
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+const root = join( currentDir, '../')
+const audioDirectory = join(root, 'audio')
+const publicDirectory = join(root, 'public')
+const utils = join(currentDir, 'utils')
+
+export default {
+  port: process.env.PORT || 3000,
+  dir: {
+    root,
+    audioDirectory,
+    publicDirectory,
+    utils,
+    songsDirectory: join(audioDirectory, 'songs'),
+    fxDirectory: join(audioDirectory, 'fx')
+  },
+  pages: {
+    homeHTML: 'home/index.html',
+    controller: 'controller/index.html'
+  },
+  location: {
+    home: '/home'
+  }
+}
--- background/src/server/index.js
+++ background/src/server/index.js
@@ -1,4 +1,6 @@
 import server from './server.js'
 import logger from '../utils/logs.js'
-server.listen(3000)
-.on('listening', () => logger.info('server running at 3000'))
+import config from '../config.js'
+
+server.listen(config.port)
+.on('listening', () => logger.info(`server running at ${config.port}`))
