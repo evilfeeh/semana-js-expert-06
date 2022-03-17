f91cdcac935605d41e874dbaaaa96d60f4e64823
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-03-17 12:57:03 -0300

    test: ensure server return a 500 if an error occured

--- src/tests/unit/server/routes.test.js
+++ src/tests/unit/server/routes.test.js
@@ -149,5 +149,21 @@ describe('Test server routes', () => {
     expect(params.response.writeHead).toHaveBeenCalledWith(404)
     expect(params.response.end).toHaveBeenCalled()
   })
+
+  test('given an error it should respond with 500', async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = 'GET'
+    params.request.url = `/unknown`
   
+    jest.spyOn(
+      Controller.prototype,
+      Controller.prototype.getFileStream.name
+    ).mockRejectedValue(new Error('Error:'))
+  
+  
+    await handler(...params.values())
+  
+    expect(params.response.writeHead).toHaveBeenCalledWith(500)
+    expect(params.response.end).toHaveBeenCalled()
+  })
 })
