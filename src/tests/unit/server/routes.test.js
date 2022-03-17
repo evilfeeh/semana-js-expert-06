1239105ded2ef20285f72f63551cfa28e6e1db7d
felipegsantos15 <felipegoncalvesdsantos@gmail.com>
2022-03-17 12:51:13 -0300

    test: ensure controller router response with controller/index.html page

--- src/tests/unit/server/routes.test.js
+++ src/tests/unit/server/routes.test.js
@@ -23,7 +23,7 @@ describe('Test server routes', () => {
     expect(params.response.end).toHaveBeenCalled()
   })
 
-  test('GET /home - should response with home/index.js with stream', async () => {
+  test('GET /home - should response with home/index.html with stream', async () => {
     const params = TestUtil.defaultHandleParams()
     params.request.method = 'GET'
     params.request.url = '/home'
@@ -43,6 +43,29 @@ describe('Test server routes', () => {
     await handler(...params.values())
   
     expect(Controller.prototype.getFileStream).toBeCalledWith(pages.homeHTML)
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response)
+  })
+
+  test('GET /controller - should response with controller/index.js with stream', async () => {
+    const params = TestUtil.defaultHandleParams()
+    params.request.method = 'GET'
+    params.request.url = '/controller'
+    const mockFileStream = TestUtil.generateReadableStream(['data'])
+    jest.spyOn(
+      Controller.prototype,
+      Controller.prototype.getFileStream.name
+    ).mockResolvedValue({
+      stream: mockFileStream
+    })
+  
+    jest.spyOn(
+      mockFileStream,
+      "pipe"
+    ).mockReturnValue()
+  
+    await handler(...params.values())
+  
+    expect(Controller.prototype.getFileStream).toBeCalledWith(pages.controllerHTML)
     expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response)
   })
 })
